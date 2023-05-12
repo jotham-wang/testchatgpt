50373b3615c8f1baeb7aa46ae362499421eeff27
justinzyw <justin.zy.wang@gmail.com>
2023-05-12 10:55:53 +0800

    added chatbot_api.py

---
+++ chatbot_api.py
@@ -0,0 +1,18 @@
+from gradio_client import Client
+import os
+
+def chatbot_api(req):
+
+    hfapi = os.environ["HFAPIKEY"]
+    hfsprepo = os.environ["HFSPREPO"]
+
+    #client = Client("http://127.0.0.1:7860")
+    client = Client(hfsprepo, hf_token=hfapi)
+    result = client.predict(
+        req,  # str representing string value in '输入需求' Textbox component
+        api_name="/predict"
+    )
+
+    return result
+
+#print(chatbot_api("test"))
